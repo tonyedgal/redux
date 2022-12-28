3809dc9e79cb03778503b1cee219b8d3072840a8
Tony Edgal <tonyedgal@gmail.com>
2022-12-28 19:11:41 +0100

    fix: fixed single post page reload issue
    - reloading single post page was throwing a `post not found` error

--- src/features/posts/PostsLists.js
+++ src/features/posts/PostsLists.js
@@ -1,38 +1,11 @@
-import { useEffect, useRef } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  selectAllPosts,
-  getPostsStatus,
-  getPostsError,
-  fetchPosts,
-} from "./postsSlice";
+import { useSelector } from "react-redux";
+import { selectAllPosts, getPostsStatus, getPostsError } from "./postsSlice";
 import PostExcerpt from "./PostExcerpt";
 
 const PostsLists = () => {
-  const dispatch = useDispatch();
-  const fetchRef = useRef(false);
-
   const posts = useSelector(selectAllPosts);
   const postsStatus = useSelector(getPostsStatus);
   const postsError = useSelector(getPostsError);
-
-  useEffect(() => {
-    if (fetchRef.current === false) {
-      if (postsStatus === "idle") {
-        dispatch(fetchPosts());
-      }
-    }
-    /**
-     * we use postsStatus and dispatch as dependencies
-     * because they were declared inside the component
-     * and we want to re-run the effect if they change.
-     * fetchPosts() was declared outside the component
-     * and so it doesn't need to be included in the dependencies array.
-     */
-    return () => {
-      fetchRef.current = true;
-    };
-  }, [posts, postsStatus, dispatch]);
 
   let content;
   if (postsStatus === "loading") {
