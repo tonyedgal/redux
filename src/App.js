35c4b76fea0ad6d0fe3e6748dfa0594ff3094bab
Tony Edgal <tonyedgal@gmail.com>
2023-01-12 23:20:27 +0100

    feat: adds users list page and user's posts page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import AddPostForm from "./features/posts/AddPostForm";
 import PostsLists from "./features/posts/PostsLists";
 import SinglePostpage from "./features/posts/SinglePostpage";
 import EditPostForm from "./features/posts/EditPostForm";
+import UsersList from "./features/users/UsersList";
+import UserPage from "./features/users/UserPage";
 import Layout from "./components/Layout";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
@@ -16,6 +18,13 @@ function App() {
           <Route path=":postId" element={<SinglePostpage />} />
           <Route path="edit/:postId" element={<EditPostForm />} />
         </Route>
+
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
   );
